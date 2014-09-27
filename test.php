20eb449afc595bf616568afc42c3fb28f8f8bc63
mckee1990 <qing110>
2014-09-28 00:10:55 +0800

    test

--- test.php
+++ test.php
@@ -6,5 +6,4 @@ echo json_encode(array(
 		'method' => $_SERVER['REQUEST_METHOD'],
         'content_type' => $_SERVER['CONTENT_TYPE'],
         'data' => json_decode(file_get_contents('php://input'), true)
-	));
-die();
+));
